72d5910e017081fe17e8a50b2dc3717037b23bd6
Nagesh Hebbar <nageshhebbar@gmail.com>
2023-07-31 14:26:59 -0400

    Initial commit

---
+++ 14/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var sol = new Solution();
+var input = new string[] {"dog","racecar","car" };
+var result = sol.LongestCommonPrefix(input);
+
+System.Console.WriteLine(result);
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        
+        char[] refernceArray = strs[0].ToCharArray();
+
+        var strsToCompare = strs.Skip(1).ToArray();
+
+        foreach(string str in strsToCompare) {
+            char[] charArray = str.ToCharArray();
+            
+            var minLength = Math.Min(charArray.Length, refernceArray.Length);
+            Console.WriteLine($"{str} " + minLength);
+            int i = 0;
+            for(i = 0; i < minLength; i++) {
+                // Console.WriteLine(i + " " + charArray[i] + " " + refernceArray[i] );
+                Console.WriteLine( charArray[i] + " " + refernceArray[i] );
+                if(charArray[i] != refernceArray[i]) {
+                    Console.WriteLine("Mismatch at " + i);
+                    refernceArray = refernceArray.Take(i).ToArray();
+                    break;
+                }
+                Console.WriteLine("outside if " + i + " " + minLength);
+            }
+
+            Console.WriteLine("outside for " + i + " " + minLength);
+
+            if( i == minLength ) {
+                Console.WriteLine("Truncating to minLength");
+                refernceArray = refernceArray.Take(minLength).ToArray();
+            }
+        }
+        
+        return new string(refernceArray);
+    }
+}
---
+++ 20/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+var sol = new Solution();
+var input = "(){}}{";
+//var input = "(){}";
+Console.WriteLine(sol.IsValid(input));
+ 
+public class Solution
+{
+    private readonly Stack<char> _parenthesesStack = new();
+    public bool IsValid(string str)
+    {
+        char[] parenthesesArray = str.ToCharArray();
+        if(parenthesesArray.Length == 1) {
+            return false;
+        }
+        foreach (char parentheses in parenthesesArray)
+        {
+            var popResult = true;
+            switch (parentheses)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    _parenthesesStack.Push(parentheses);
+                    break;
+                case ')': { popResult = _parenthesesStack.Count > 0 && _parenthesesStack.Pop() == '('; break; };
+                case ']': { popResult = _parenthesesStack.Count > 0 && _parenthesesStack.Pop() == '['; break; };
+                case '}': { popResult = _parenthesesStack.Count > 0 && _parenthesesStack.Pop() == '{'; break; };
+                default: return false;
+            }
+
+            if(!popResult) { return false;}
+        }
+
+        return _parenthesesStack.Count == 0;
+    }
+}
